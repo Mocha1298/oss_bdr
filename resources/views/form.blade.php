f9a59435c7c7a3037175742d0d9a6576ab2b435a
Arifin <moch.n.arifin@gmail.com>
2023-08-02 23:03:52 +0700

    menambah validation

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -7,24 +7,26 @@
 
 <head>
     <title>BDR OSS</title>
-    <link rel="shortcut icon" href="/ico.png" />
+    <link rel="shortcut icon" href="/OSS_LOGO.png" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
         integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="form.css">
+    {{-- <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="demo.css" />
+    <link rel="stylesheet" type="text/css" href="component.css" /> --}}
 </head>
 
-<body
-    style="background: rgba(241, 241, 241, 0.603);background: url(background.png) bottom no-repeat;background-size: 120%">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4" style="background: #243460 !important">
-        <a style="background: none; width:100%;height:50px" class="navbar-brand text-center" href="#"><img
-                src="/IHM-putih.png" class="img-fluid mb-1" alt="Responsive image" style="width: 100px"></a>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        {{-- <a style="background: none; width:100%;height:50px" class="navbar-brand text-center" href="#"><img
+                src="/IHM-putih.png" class="img-fluid mb-1" alt="Responsive image" style="width: 100px"></a> --}}
+        <a style="background: none; width:100%" class="navbar-brand" href="#"><img src="/LOGO OSS1.png"
+                class="img-fluid" alt="Responsive image"></a>
     </nav>
     <div class="container mt-1 mb-5">
-        <a style="background: none; width:100%" class="navbar-brand d-flex justify-content-center mb-4"
-            href="#"><img src="/LOGO OSS.png" class="img-fluid" alt="Responsive image" style="width:200px"></a>
         <div class="card">
             <div class="card-header" style="background: #0092ab !important">
                 <div class="header-title">
@@ -32,21 +34,23 @@
                 </div>
             </div>
             <div class="card-body" style="background: rgba(241, 241, 241, 0.603);">
-                <p class="sub-input" style="font-family: font-itlc"><span class="wajib">(*)</span> Wajib di isi</p>
                 <form action="/post-form" method="post" enctype="multipart/form-data">
                     @csrf
                     <div class="row">
                         <div class="form-group col-md-12">
-                            <label for="inc_name">Nama Instansi<span class="wajib"> *</span></label>
-                            <input id="inc_name" type="text" name="inc_name" required />
+                            <label for="inc_name">Nama Instansi<span class="wajib"> (wajib diisi)</span></label>
+                            <input id="inc_name" type="text" name="inc_name" required
+                                onkeyup="validateText('inc_name')" onclick="validateText('inc_name')"
+                                onchange="validateText('inc_name')" />
+                            <p class="inc_name"></p>
                             <p class="sub-input">Nama instansi, contoh: PT xxx / CV xxx</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-md-12">
-                            <label for="#">Jenis Instansi<span class="wajib"> *</span></label><br>
-                            <label class="containeradio">Negeri
-                                <input type="radio" name="inc_type" value="Negeri" onclick="hideother()">
+                            <label for="#">Jenis Instansi<span class="wajib"> (wajib diisi)</span></label><br>
+                            <label class="containeradio">Pemerintah
+                                <input type="radio" name="inc_type" value="Pemerintah" onclick="hideother()" checked>
                                 <span class="checkmark"></span>
                             </label>
                             <label class="containeradio">Swasta
@@ -63,54 +67,69 @@
                     </div>
                     <div class="row">
                         <div class="form-group col-md-12">
-                            <label for="pic">PIC<span class="wajib"> *</span></label>
-                            <input id="pic" type="text" name="pic" required />
+                            <label for="pic">PIC<span class="wajib"> (wajib diisi)</span></label>
+                            <input id="pic" type="text" name="pic" required onkeyup="validateText('pic')"
+                                onclick="validateText('pic')" onchange="validateText('pic')" />
+                            <p class="pic" style="display: none"></p>
                             <p class="sub-input">Nama penanggung jawab dari pihak instansi.</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-md-12">
-                            <label for="no_pic">Phone<span class="wajib"> *</span></label>
-                            <input id="no_pic" type="number" name="no_pic" required />
+                            <label for="no_pic">Phone<span class="wajib"> (wajib diisi)</span></label>
+                            <input id="no_pic" type="number" name="no_pic" required
+                                onkeyup="validateText('no_pic')" onclick="validateText('no_pic')"
+                                onchange="validateText('no_pic')" />
+                            <p class="no_pic" style="display: none"></p>
                             <p class="sub-input">Kontak yang bisa dihubungi, contoh: 08xxxx.</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-md-12">
-                            <label for="email_pic">Email<span class="wajib"> *</span></label>
-                            <input id="email_pic" type="email" name="email_pic" required />
+                            <label for="email_pic">Email<span class="wajib"> (wajib diisi)</span></label>
+                            <input id="email_pic" type="email" name="email_pic" required
+                                onkeyup="validateText('email_pic')" onclick="validateText('email_pic')"
+                                onchange="validateText('email_pic')" />
+                            <p class="email_pic" style="display: none"></p>
                             <p class="sub-input">Contoh: user@website.com.</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-md-12">
-                            <label for="people">Jumlah Orang<span class="wajib"> *</span></label>
-                            <input id="people" type="number" name="people" required />
+                            <label for="people">Jumlah Orang<span class="wajib"> (wajib diisi)</span></label>
+                            <input id="people" type="number" name="people" required
+                                onkeyup="validateText('people')" onclick="validateText('people')"
+                                onchange="validateText('people')" />
+                            <p class="people" style="display: none"></p>
                             <p class="sub-input">Jumlah orang dari instansi yang akan berkunjung.</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-md-12 item">
                             <label for="">Rencana Kunjungan</label>
-                            <label for="plan_date">Tanggal<span class="wajib"> *</span></label>
-                            <input id="plan_date" type="date" name="plan_date" required
-                                min="{{ $date }}" />
+                            <label for="plan_date">Tanggal<span class="wajib"> (wajib diisi)</span></label>
+                            <input id="plan_date" type="date" name="plan_date" required min="{{ $date }}"
+                                onkeyup="validateText('plan_date')" onclick="validateText('plan_date')"
+                                onchange="validateText('plan_date')" />
+                            <p class="plan_date" style="display: none"></p>
                             <i class="fas fa-calendar-alt"></i>
                         </div>
                     </div>
                     <div class="row">
                         <div class="form-group col-md-12 item">
-                            <label for="plan_date">Jam<span class="wajib"> *</span></label>
-                            <input id="plan_time" type="time" name="plan_time" required />
+                            <label for="plan_date">Jam<span class="wajib"> (wajib diisi)</span></label>
+                            <input id="plan_time" type="time" name="plan_time" required
+                                onkeyup="validateText('plan_time')" onclick="validateText('plan_time')"
+                                onchange="validateText('plan_time')" />
+                            <p class="plan_time" style="display: none"></p>
                             <i class="fas fa-clock"></i>
                         </div>
                     </div>
+                    <label class="mt-2 dokumen" for="dokumen">Dokumen Pendukung<span class="wajib"> (wajib
+                            diisi)</span></label>
                     <div class="row" id="dokumen">
-                        <div class="col-lg-11 col-10">
-                            <div class="form-group" id="item">
-                                <label for="dokumen">Dokumen Pendukung<span class="wajib"> *</span></label>
-                                <input id="1" type="file" name="dokumen[]" required onchange="cek(1)">
-                            </div>
+                        <div class="form-group col-lg-11 col-10 item">
+                            <input id="1" type="file" name="dokumen[]" required onchange="cek(1)">
                         </div>
                         <div id="add" class="col-lg-1 col-2">
                             <a class="tombol add" onclick="add()"><i style="color: #fff" class="fa fa-plus"></i></a>
@@ -236,7 +255,7 @@
             box_input += "<div class='form-group' id='item'>"
             box_input += "<input id='" + number + "' type='file' name='dokumen[]' required onchange='cek(" + number +
                 ")'></div></div>"
-            box_input += "<div id='add tambahan' class='col-lg-1 col-2' style='padding: 10px 2px'>"
+            box_input += "<div id='add' class='col-lg-1 col-2'>"
             box_input += "<a class='tombol add' onclick='hapus(" + number +
                 ")'><i style='color: #fff'class='fa fa-minus'></i></a></div>"
             $('.add_dokumen').append(box_input);
@@ -311,6 +330,39 @@
             var input = $("#inc_type");
             input.prop('required', false);
             input.hide();
+        }
+
+        function validateText(id) {
+            let field = document.getElementById(id);
+            let flag = document.getElementsByClassName(id)[0];
+            let opacity = 0;
+            if (field.value == '') {
+                field.style.borderColor = "red";
+                flag.innerHTML = "field tidak boleh kosong";
+                flag.style.color = "red";
+                flag.style.fontStyle = "italic";
+                flag.style.fontSize = "90%";
+                flag.style.display = "block";
+            } else {
+                field.style.borderColor = "#ccc";
+                flag.innerHTML = "";
+                flag.style.display = "none";
+                var validRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+                if (id == 'email_pic') {
+                    if (!field.value.match(validRegex)) {
+                        field.style.borderColor = "red";
+                        flag.innerHTML = "format email tidak valid";
+                        flag.style.color = "red";
+                        flag.style.fontStyle = "italic";
+                        flag.style.fontSize = "90%";
+                        flag.style.display = "block";
+                    } else {
+                        field.style.borderColor = "#ccc";
+                        flag.innerHTML = "";
+                        flag.style.display = "none";
+                    }
+                }
+            }
         }
         // alert('Your screen resolution is ' + screen.width + 'x' + screen.height);
     </script>
